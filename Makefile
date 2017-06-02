3960f8a8eb184bc1b2b7f9202d23dcf155e0a3fb
Fede <federicoagustincaccia@gmail.com>
2017-06-02 16:47:58 -0300

    first structure

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Macros
+MAIN_DIR=.
+DEP_DIR=$(MAIN_DIR)/inc
+SRC_DIR=$(MAIN_DIR)/src
+OBJ_DIR=$(MAIN_DIR)/obj
+BIN_DIR=$(MAIN_DIR)/bin
+OBJELEM = $(OBJ_DIR)/main.o
+EXE=$(BIN_DIR)/newton
+MPICC=mpic++
+
+# Reglas explicitas
+all: $(BIN_DIR) $(OBJ_DIR) $(OBJELEM)
+	$(MPICC) $(OBJELEM) -o $(EXE)
+
+#~ 	$(FC) $(OBJELEM) \
+#~ 	$(FFLAGS) -o $(EXE) \
+#~ 	$(PETSC_FORTRAN_LIB) $(PETSC_LIB) ${SLEPC_SYS_LIB} -lz
+
+clean_:
+	$(RM) $(OBJELEM) $(OBJELEMTEST) $(EXE) $(EXETEST)
+
+# Creacion de objetos
+${OBJ_DIR}/%.o: ${SRC_DIR}/%.cpp
+	$(MPICC) -c -o $@ $< -I${DEP_DIR}
+#~ 	${PETSC_COMPILE} -c ${CFLAGS} -o $@ $< -I${DEP_DIR}
+
+#~ include ${PETSC_DIR}/lib/petsc/conf/variables
+#~ include ${SLEPC_DIR}/lib/slepc/conf/slepc_common
--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,31 @@
+#undef __FUNCT__
+#define __FUNCT__ "main"
 
+int main(int argc,char **argv)
+{  
+  
+  NewtonFeatures newt();
+  newt.init() // MPI
+  
+  ParserClass* Parser = new ParserClass();
+  error = Parser->parseFile();checkError();
+  
+  SystemClass* System = new SystemClass();
+  error = System->initialize();checkError(); // System generates Code, estruturas de output que chequea solver
+  
+  EvoltutionClass* Evolution = new EvoltutionClass();
+  error = Evolution->start();checkError();
+  
+  SolverClass* Solver = new SolverClass();
+  error = Solver->initialize();checkError(); // inicilaiza residuals, comunicadores, etc.
+  
+  while((*Evolution.status != END)){    
+    error = Solver->iterateUntilConvergence();checkError();
+    error = Evolution->update();checkError();
+  }
+  
+  Solver->finish();
+  
+  newt.fini()//MPI
+  
+}
