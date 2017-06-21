3112306c96116950c82c74684d344bbb6ee95c02
Fede <federicoagustincaccia@gmail.com>
2017-06-21 01:36:57 -0300

    adding license

--- README.md
+++ README.md
@@ -104,3 +104,20 @@ Applications, 7-11 November 2016, Córdoba, Argentina.](https://goo.gl/mZ3A7o)
 
 ## Contact
 Any questions or suggestions feel free to contact me at [federicoagustincaccia@gmail.com]
+
+## Licensing
+
+Copyright (C) 2017 Federico A. Caccia
+
+Newton is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Newton is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
--- inc/global.h
+++ inc/global.h
@@ -11,8 +11,26 @@ maste code            |
 
 Global sets global defines, variables and functions commonly used.
 
-Author: Federico A. Caccia
 Date: 3 June 2017
+
+-------------------------------------------------------------------------------
+
+Copyright (C) 2017 Federico A. Caccia
+
+This file is part of Newton.
+
+Newton is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Newton is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 \*****************************************************************************/
 
