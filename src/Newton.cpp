2e1ac0ea1cb470022f1355692a172f9852bc2802
Fede <federicoagustincaccia@gmail.com>
2017-06-21 01:50:56 -0300

    updating license

--- src/Newton.cpp
+++ src/Newton.cpp
@@ -33,7 +33,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with sNewton.  If not, see <http://www.gnu.org/licenses/>.
 
 \*****************************************************************************/
 
