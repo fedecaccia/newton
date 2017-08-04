98acd2df841c2869bc589e160ce04b534936e5aa
Fede <federicoagustincaccia@gmail.com>
2017-08-04 15:30:06 -0300

    just one process of newton in mpi comm example

--- examples/linear_system_2_clients/mpi_comm-mpi_comm/run.sh
+++ examples/linear_system_2_clients/mpi_comm-mpi_comm/run.sh
@@ -1,2 +1,2 @@
 hostname > mf
-mpirun -np 2 -machinefile mf ${NEWTON_DIR}/bin/Newton : -np 1 ${NEWTON_DIR}/usr/bin/Test 0 0 mpi_comm 1 : -np 1 ${NEWTON_DIR}/usr/bin/Test 0 1 mpi_comm 2
+mpirun -np 1 -machinefile mf ${NEWTON_DIR}/bin/Newton : -np 1 ${NEWTON_DIR}/usr/bin/Test 0 0 mpi_comm 1 : -np 1 ${NEWTON_DIR}/usr/bin/Test 0 1 mpi_comm 2
